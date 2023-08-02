0687db01ac855b9833355ca5c44861b395b6d772
Svatosik <125908175+Svatosik@users.noreply.github.com>
2023-08-02 20:29:49 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const getGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" 
+         src="${preview}" 
+         alt="${description}" />
+      </a>
+   </li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', getGallery);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
